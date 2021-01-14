0a1ef6b9680c657de6673aff32378ca422bc53a2
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:12:07 +0200

    Add files via upload

---
+++ Task05_Special_Numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task05_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int num = 1; num <= n; num++)
+
+            {
+
+                int sumOfDigits = 0;
+
+                int digits = num;
+
+                while (digits > 0)
+
+                {
+
+                    sumOfDigits += digits % 10;
+
+                    digits = digits / 10;
+
+                }
+
+                // TODO: check whether the sum is special
+
+                if(sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+                {
+                    Console.WriteLine($"{num} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{num} -> False");
+                }
+
+            }
+        }
+    }
+}
