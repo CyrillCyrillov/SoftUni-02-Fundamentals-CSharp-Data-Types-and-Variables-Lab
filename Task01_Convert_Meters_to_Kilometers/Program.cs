f00a1be389dfa91f4363dc803fc2f0596032e0b4
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:09:19 +0200

    Add files via upload

---
+++ Task01_Convert_Meters_to_Kilometers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task01_Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int meters = int.Parse(Console.ReadLine());
+            float kilometers = meters / 1000.0f;
+
+            Console.WriteLine($"{kilometers:F2}");
+            */
+
+            int meters = int.Parse(Console.ReadLine());
+
+            float kilometers = meters / 1000.0f;
+
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
