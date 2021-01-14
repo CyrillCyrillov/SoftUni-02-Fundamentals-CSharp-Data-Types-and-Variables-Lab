80b85e3c51842780bb3525f1be8fd0d8e613ec1e
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:18:31 +0200

    Add files via upload

---
+++ Task11_Refactor_Special_Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task12_Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            //int obshto = 0;
+            //int takova = 0;
+            //bool toe = false;
+            for (int i = 1; i <= number; i++)
+            {
+                int nextNumber = i;
+                int sum = 0;
+                while (nextNumber > 0)
+                {
+                   sum += nextNumber % 10;
+                    nextNumber = nextNumber / 10;
+                }
+                
+                if(sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+
+        }
+    }
+}
