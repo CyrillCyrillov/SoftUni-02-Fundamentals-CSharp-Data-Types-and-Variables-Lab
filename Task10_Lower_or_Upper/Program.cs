f8025714dc9dc95a877b786426f689e5976d06fb
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:16:48 +0200

    Add files via upload

---
+++ Task10_Lower_or_Upper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task10_Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char input = char.Parse(Console.ReadLine());
+
+            if(64 <= input && input <= 90)
+            {
+                Console.WriteLine("upper-case");
+            }
+
+            if (97 <= input && input <= 122)
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
