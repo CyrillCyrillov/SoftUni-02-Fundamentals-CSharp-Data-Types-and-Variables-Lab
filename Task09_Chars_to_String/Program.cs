f6308b97e78f9e92bfd8b6fd0b5f749287271ee3
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:15:47 +0200

    Add files via upload

---
+++ Task09_Chars_to_String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task09_Chars_to_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            char third = char.Parse(Console.ReadLine());
+
+            string stringOfchars = first.ToString() + second.ToString() + third.ToString();
+
+
+            Console.WriteLine(stringOfchars);
+        }
+    }
+}
