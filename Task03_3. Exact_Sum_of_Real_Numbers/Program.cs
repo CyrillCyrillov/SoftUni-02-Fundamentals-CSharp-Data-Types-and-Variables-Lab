1ca12c983dda1cf2c2ae6e49f38d5ac4f82ab94d
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:10:18 +0200

    Add files via upload

---
+++ Task03_3. Exact_Sum_of_Real_Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task03_3._Exact_Sum_of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                sum = sum + decimal.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+
+
+
+        }
+    }
+}
