c37ecdc92919415546056df06a1f621d8170742b
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:07:05 +0200

    Add files via upload

---
+++ Task02_Pounds_to_Dollars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task02_Pounds_to_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            double britushPounds = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{britushPounds * 1.31:f3}");
+            */
+
+            double num = double.Parse(Console.ReadLine());
+
+            double result = num * 1.31;
+
+            Console.WriteLine($"{result:f3}");
+        }
+    }
+}
