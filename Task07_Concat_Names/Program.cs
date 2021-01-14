436a0430c77c31c6152619304fb10f1f0fd68f21
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-14 21:13:48 +0200

    Add files via upload

---
+++ Task07_Concat_Names/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task07_Concat_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+
+            string lastName = Console.ReadLine();
+
+            string delimiter = Console.ReadLine();
+
+            string result = firstName + delimiter + lastName;
+
+            Console.WriteLine(result);
+        }
+    }
+}
